50eb4d1658e764b0170951533ed2e8fbbd590cf1
Edwin J <hungrylulu8@gmail.com>
2023-05-15 18:37:18 -0700

    removed unncessary .fetchall()

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ generate_parquet_from_csv() # Converts csv to parquet
 # Seed it
 conn = duckdb.connect(database=settings.db_path, read_only=False)
 conn.execute(f"drop view if exists {settings.requests}")
-conn.execute(f"create view {settings.requests} as select * from '{settings.parquet_data_path}/*.parquet'").fetchall()
+conn.execute(f"create view {settings.requests} as select * from '{settings.parquet_data_path}/*.parquet'")
 
 # Query it
 res = conn.execute(f"select * from requests limit 1").fetchall()
